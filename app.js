005f38a7e99b8ea7e8eb605ac18816ad70c5e57f
evgeniy balagurov <evgeniy.balagurov@yandex.ru>
2022-09-29 23:11:06 +0400

    feat: создать точку входа app.js, сервер express, подключение к mongo

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+const connect = async (next) => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/mestodb', {
+      useNewUrlParser: true,
+      useUnifiedTopology: false,
+    });
+    console.log('MongoDB connected!');
+
+    await app.listen(PORT);
+    console.log(`App listening on port ${PORT}`);
+  } catch (err) {
+    next(new Error('Filed to connect:', err.message));
+  }
+};
+
+connect();
