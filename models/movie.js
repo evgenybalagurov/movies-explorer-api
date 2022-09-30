26a0c30f132808cb961588b494dce2701470bed7
Evgeniy <evgeniy.balagurov@yandex.ru>
2022-09-30 10:20:50 +0400

    feat: создать схему и модель movie

---
+++ models/movie.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const movieSchema = new mongoose.Schema({
+  county: {
+    type: String,
+    required: [true, 'The field "county" must be filled'],
+  },
+  director: {
+    type: String,
+    required: [true, 'The field "director" must be filled'],
+  },
+  duration: {
+    type: Number,
+    required: [true, 'The field "duration" must be filled'],
+  },
+  year: {
+    type: String,
+    required: [true, 'The field "year" must be filled'],
+  },
+  description: {
+    type: String,
+    required: [true, 'The field "description" must be filled'],
+  },
+  image: {
+    type: String,
+    required: [true, 'The field "image" must be filled'],
+    validate: [validator.isUrl, 'Invalid Url address'],
+  },
+  trailerLink: {
+    type: String,
+    required: [true, 'The field "trailerLink" must be filled'],
+    validate: [validator.isUrl, 'Invalid Url address'],
+  },
+  thumbnail: {
+    type: String,
+    required: [true, 'The field "thumbnail" must be filled'],
+    validate: [validator.isUrl, 'Invalid Url address'],
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: String, //TODO: id фильма, который содержится в ответе сервиса MoviesExplorer
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: [true, 'The field "nameRU" must be filled'],
+  },
+  nameEN: {
+    type: String,
+    required: [true, 'The field "nameEN" must be filled'],
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
