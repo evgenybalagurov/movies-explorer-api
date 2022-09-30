4dd5a682e230df9ded8461b4bf129970d096f00f
evgeniy balagurov <evgeniy.balagurov@yandex.ru>
2022-09-30 19:08:30 +0400

    feat: добавить контроллер movies

---
+++ controllers/movies.js
@@ -0,0 +1,44 @@
+const Movie = require('../models/movie');
+
+const getMovies = async (req, res, next) => {
+  try {
+    const movies = await Movie.find({})
+    return res.send(movies);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+const createMovie = async (req, res, next) => {
+  const { owner, ...movieData } = req.body;
+  try {
+    const movie = await Movie.create({ owner: req.user._id, ...movieData });
+    return res.status(201).send(movie);
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      return next(new ValidationError('Validation error. Incorrect data sent'));
+    }
+    return next(err);
+  }
+};
+
+const deleteMovie = async (req, res, next) => {
+  const { movieId } = req.params;
+
+  try {
+    const movie = await Movie.findById(movieId);
+    if (!movie) {
+      return next(new NotFoundError('This movie does not exist'));
+    }
+    if (req.user._id !== movie.owner.toString()) {
+      return next(new ForbiddenError('This movie is another user'));
+    }
+    await movie.remove();
+    return res.send(movie);
+  } catch (err) {
+    if (err.name === 'CastError') {
+      return next(new CastError('Invalid movie id'));
+    }
+    return next(err);
+  }
+};
