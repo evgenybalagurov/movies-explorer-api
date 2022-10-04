c0f834023df24c629fed6b866f06238e7bbf6477
Evgeniy <evgeniy.balagurov@yandex.ru>
2022-10-04 08:25:51 +0400

    fix: убрать ошибку валидации Joi

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -41,7 +41,7 @@ const validateCreateMovie = celebrate({
 
 const validateMovieById = celebrate({
   params: Joi.object().keys({
-    _id: Joi.objectId().required(),
+    _id: Joi.string().hex().length(24).required(),
   }),
 });
 
